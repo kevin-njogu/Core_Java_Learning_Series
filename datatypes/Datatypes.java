ab352bd79b6af18c594d30a7f585fdde37f3f7b5
jones <dummyjones254@gmail.com>
2025-10-29 17:54:16 +0300

    Show java primitive datatypes

---
+++ datatypes/Datatypes.java
@@ -0,0 +1,23 @@
+/**
+ * One main classification of datatypes is Primitive Datatypes
+ * 1. Integers - int(4 bytes), byte(1 byte), long(8 bytes), short(2 bytes)
+ * 2. Floats - double(8 bytes), floats(4 bytes)
+ * 3. Character(2 bytes) - Single character
+ * 4. Booleans - True or False
+ */
+class DataTypes {
+
+    byte b = 125;
+    short s = 8000;
+    int i = 30000;
+    long l = 598000l;
+
+    float f = 6.5f;
+    double d = 6.5;
+
+    char k = 'l';
+    char n = '8';
+
+    boolean t = true;
+    boolean z = false;
+}
