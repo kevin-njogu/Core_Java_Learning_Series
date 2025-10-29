bd8ddc00dca9d861adb98e8f043fbfb4a13d800e
jones <dummyjones254@gmail.com>
2025-10-29 15:57:01 +0300

    Show variables declaration and assignment

---
+++ variables/Variables.java
@@ -0,0 +1,12 @@
+class Variable {
+
+    static void main(String[] args) {
+        /**
+         * Defining variables and assigning
+         */
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("The sum is " + sum);
+    }
+}
