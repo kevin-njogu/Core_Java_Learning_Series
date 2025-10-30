67787b6a3c4b59068a36f9bd405ebd154bc547c3
jones <dummyjones254@gmail.com>
2025-10-30 19:08:15 +0300

    Show functional interface

---
+++ 7_functional_interfaces/FunctionalInterfaces.java
@@ -0,0 +1,8 @@
+/**
+ * Functional intefaces are java interfaces which have exactly one abstract method
+ * It is defined with @FunctionalInterface annotation
+ */
+class FunctionalInterface {
+    static void main(String[] args) {
+    }
+}
